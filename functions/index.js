44b84830d6cd4fcfef7010c624a72a2b365c31a6
ryanguai <e0694442@u.nus.edu>
2023-01-15 17:59:01 +0800

    Add todo APIs

--- functions/index.js
+++ functions/index.js
@@ -7,5 +7,20 @@ const {
     getAllTodos
 } = require('./APIs/todos')
 
+const {
+    postOneTodo
+} = require('./APIs/todos')
+
+const {
+    deleteTodo
+} = require('./APIs/todos')
+
+const {
+    editTodo
+} = require('./APIs/todos')
+
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
 exports.api = functions.https.onRequest(app);
