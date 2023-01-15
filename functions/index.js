e3b75fc0a1a4d914d80c5a8c68904af3d9578dc1
ryanguai <e0694442@u.nus.edu>
2023-01-15 18:59:42 +0800

    set up user signup API

--- functions/index.js
+++ functions/index.js
@@ -19,8 +19,19 @@ const {
     editTodo
 } = require('./APIs/todos')
 
+
+const {
+    loginUser
+} = require('./APIs/users')
+
+const {
+    signUpUser
+} = require('./APIs/users')
+
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyAWgO4kEn2CdS5G3wdDaf6IWuowkPGK7Iw",
+  authDomain: "todoapp-65153.firebaseapp.com",
+  projectId: "todoapp-65153",
+  storageBucket: "todoapp-65153.appspot.com",
+  messagingSenderId: "171978637376",
+  appId: "1:171978637376:web:858628ce795578c2245fa5",
+  measurementId: "G-G21H8NH1L8"
+};
