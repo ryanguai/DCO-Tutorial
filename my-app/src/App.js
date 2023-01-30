9f7a33103cf6e37570d68d6b421d9887f61508fd
ryanguai <e0694442@u.nus.edu>
2023-01-30 18:51:32 +0800

    Login page done, but unable to firebase deploy

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login';
+function App() {
+  return (
+    <Router>
+        <div>
+           <Switch>
+               <Route exact path="/login" component={login}/>
+           </Switch>
+        </div>
+    </Router>
+  );
+}
+export default App;
