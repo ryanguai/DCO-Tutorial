43d76285195c2b6676f91546db964b45253e397e
ryanguai <e0694442@u.nus.edu>
2023-02-05 00:25:26 +0800

    Complete signup page

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -27,33 +27,6 @@ const theme = createTheme({
     	}
    });
 
-const styles = (theme) => ({
-	paper: {
-		marginTop: theme.spacing(8),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center'
-	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main
-	},
-	form: {
-		width: '100%',
-		marginTop: theme.spacing(1)
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2)
-	},
-	customError: {
-		color: 'red',
-		fontSize: '0.8rem',
-		marginTop: 10
-	},
-	progess: {
-		position: 'absolute'
-	}
-});
 
 class login extends Component {
 	constructor(props) {
